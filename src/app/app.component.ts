58dd9dd78fe947aa69314f67cd190ef80dff2621
Anselmo <ansinftec17@gmail.com>
2022-04-03 11:43:13 -0300

    Adicionando arquivos padrao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'step';
+
+  steps = [
+    { active: true, name: 'grupo' },
+  ]
+
+  message() {
+    this.steps = [
+      { active: true, name: 'grupo' },
+      { active: true, name: 'message' },
+    ];
+  }
+
+  custo() {
+    this.steps = [
+      { active: true, name: 'grupo' },
+      { active: true, name: 'message' },
+      { active: true, name: 'custo' },
+    ];
+  }
+
+  timeline() {
+    this.steps = [
+      { active: true, name: 'grupo' },
+      { active: true, name: 'message' },
+      { active: true, name: 'custo' },
+      { active: true, name: 'timeline' },
+    ];
+  }
+
+  voltar() {
+    this.steps = [
+      { active: true, name: 'grupo' },
+    ];
+  }
 }
